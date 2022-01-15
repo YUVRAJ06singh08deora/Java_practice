75f336d10ae8e9a8ec71b7b9da4a8c01e5640755
YUVRAJ06singh08deora <06yuvraj2001singh@gmail.com>
2022-01-15 19:51:31 +0530

    Integer Between String

---
+++ src/Integer_btw_string.java
@@ -0,0 +1,6 @@
+public class Integer_btw_string {
+    public static void main(String arg[]){
+//        System.out.println(2+2 +"="+2+2);  to print 4=22
+        System.out.println(2+2+"="+(2+2));
+    }
+}
