29c80efa087a81879265645d513a106cf4548547
YUVRAJ06singh08deora <06yuvraj2001singh@gmail.com>
2022-01-15 19:40:00 +0530

    Program to Take integer value

---
+++ src/Program_to_take_input_int.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Program_to_take_input_int {
+    public static void main(String arg[]){
+        int a;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the value for x");
+        a=s.nextInt();
+        System.out.println("The Entered value is "+a);
+
+    }
+}
