f8aee3b02b4023af23e026ae911dadbc87f9d6bd
YUVRAJ06singh08deora <06yuvraj2001singh@gmail.com>
2022-01-16 09:33:16 +0530

    Swaaping using third variable

---
+++ src/Swapping_2_integers_with_third_variable.java
@@ -0,0 +1,11 @@
+public class Swapping_2_integers_with_third_variable {
+    public static void main(String[] args){
+        int a=24;
+        int b=45;
+        int c;
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("Value of a"+a+"Value of b"+b);
+    }
+}
