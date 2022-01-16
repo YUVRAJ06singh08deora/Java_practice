f39ee0fee58ee2eeaf6c6b3556d98f3ba762d09e
YUVRAJ06singh08deora <06yuvraj2001singh@gmail.com>
2022-01-16 09:36:42 +0530

    Swaaping without third variable

---
+++ src/Swapping_without_thord_variable.java
@@ -0,0 +1,10 @@
+public class Swapping_without_thord_variable {
+    public static void main(String[] args){
+        int a=45;
+        int b=34;
+        a=a+b;
+        b=a-b;
+        a=-(b-a);
+        System.out.println("The value of a"+a+"value of b"+b);
+    }
+}
